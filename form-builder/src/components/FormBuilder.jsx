e5e0bac9fae567f0c8a7f49c7e4e343209bb6a2e
Firas FAZ. Abuzahra <fabuzahra@seapay.local>
2023-08-14 17:46:35 +0300

    added some validation

--- form-builder/src/components/FormBuilder.jsx
+++ form-builder/src/components/FormBuilder.jsx
@@ -1,20 +1,108 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function FormBuilder({form}) {
+function FormBuilder({form, handleChange, handleSubmit}) {
+  const [errors, setErrors] = useState({})
+  
+  const handleValidation = (e, {id, required, type, regex}) => {
+    let formIsValid = true;
+    const userInput = e.target.value
+
+    if (required && !e.target.value) {
+      formIsValid = false;
+      setErrors({[id]: "Cannot be empty"});
+    }
+
+    // Name
+    if (type === "name") {
+      if (userInput.match(regex)) {
+        formIsValid = true;
+        setErrors({[id]: null});
+        console.log('valid')
+      } else {
+        formIsValid = false;
+        setErrors({[id]: "Only letters"});
+        console.log('invalid')
+      }
+    }
+
+    // Email
+    if (type === "email") {
+      let lastAtPos = userInput.lastIndexOf("@");
+      let lastDotPos = userInput.lastIndexOf(".");
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          userInput.indexOf("@@") == -1 &&
+          lastDotPos > 2 &&
+          userInput.length - lastDotPos > 2
+        )
+      ) {
+        formIsValid = false;
+        setErrors({[id]: "Email is not valid"});
+      } else {
+        formIsValid = true
+        setErrors({[id]: null})
+      }
+    }
+
+    return formIsValid;
+  }
   return (
     <>
       {form.components.map((component, index) => (
           <div key={index} className={`${form.class}`}>
-            <label htmlFor={component.id} className={component.labelClass}>{component.label}</label>
             {
-              component.type === 'select' ? (
-                <select className={component.class}>
-                  {component.options.map((option, index) => (
-                    <option key='index' value={option.value}>{option.text}</option>
-                  ))}
-                </select>
+              component.type === 'submit' ? (
+                <button id={component.id} className={component.class} type={component.type} onClick={handleSubmit}>{component.text}</button>
               ) : (
-                <input id={component.id} className={component.class} type={component.type} />
+                component.type === 'select' ? (
+                  <>
+                    <label
+                      htmlFor={component.id} 
+                      className={component.labelClass}
+                    >
+                      {component.label}
+                    </label>
+                    <select
+                      id={component.id} 
+                      className={component.class} 
+                      onClick={(e) => {
+                        handleChange(e); 
+                        handleValidation(component.required) 
+                      }}
+                    >
+                      {
+                        component.options.map((option, index) => (
+                          <option key={index} value={option.value}>{option.text}</option>
+                        ))
+                      }
+                    </select>
+                  </>
+                ) : (
+                  <>
+                    <label 
+                      htmlFor={component.id} 
+                      className={component.labelClass}
+                    >
+                      {component.label}
+                    </label>
+                    <input 
+                      id={component.id} 
+                      className={component.class} 
+                      type={component.type} 
+                      onChange={(e) => { 
+                        handleChange(e); 
+                        handleValidation(e, component) 
+                      }} 
+                    />
+                    {component.id === 'name1' ? 
+                      <span style={{ color: "red" }}>{errors["name"]}</span> : 
+                      <span style={{ color: "red" }}>{errors["email"]}</span>
+                    }
+                  </>
+                )
               )
             }
           </div>
--- form-builder/src/components/TestForm.jsx
+++ form-builder/src/components/TestForm.jsx
@@ -1,18 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import FormBuilder from './FormBuilder'
 function TestForm() {
+    const [formData, setFormData] = useState({})
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value
+        })
+    }
+    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData)
+    }
+    
     const customForm = [
+        {
+            'id': 'name1',
+            'class': 'customForm',
+            'label': 'name',
+            'labelClass': 'label',
+            'type': 'name',
+            'required': true,
+            'value': '',
+            'regex': '^[a-zA-Z]+$'
+        },
         {
             'id': 'input1',
             'class': 'customForm',
             'label': 'input1',
             'labelClass': 'label',
+            'type': 'text',
+            'required': false,
+            'value': ''
         },
         {
-            'id': 'input2',
+            'id': 'email',
             'class': 'customForm',
-            'label': 'input2',
-            'labelClass': 'label'
+            'label': 'email',
+            'labelClass': 'label',
+            'type': 'email',
+            'required': false,
+            'value': ''
         },
         {
             'id': 'select',
@@ -24,14 +54,40 @@ function TestForm() {
                 'value': 'select element',
                 'text': 'select element'
             }],
-        } 
+            'required': false,
+            'value': ''
+        },
+        {
+            'id': 'submit',
+            'class': 'submitBtn',
+            'type': 'submit',
+            'text': 'Submit',
+            'required': false,
+            'value': '',
+            'visible': true,
+
+        }
     ]
+
+    // useEffect(() => {
+    //     customForm.map((el, index) => (
+    //         // ...formData,
+    //         // setFormData({
+    //         //     [el.id]: '',
+    //         // })
+    //         console.log(el.id)
+    //     ))
+    // }, [])
   return (
     <div>
-        <FormBuilder form={{
-            class: 'form',
-            components: customForm
-        }} />
+        <FormBuilder 
+            form={{
+                class: 'form',
+                components: customForm
+            }}
+            handleChange={handleChange}
+            handleSubmit={handleSubmit}
+         />
     </div>
   )
 }
